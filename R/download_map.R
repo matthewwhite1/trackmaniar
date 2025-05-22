61bbaf1f10a3d35df95c34f891591794f9a7d9d3
matthewwhite1 <mrmatthewwins@gmail.com>
2025-05-21 20:02:09 -0600

    Made progress on search function

--- R/download_map.R
+++ R/download_map.R
@@ -1,18 +1,20 @@
 #' @export
-download_map <- function(id) {
-  # Define url and output path
-  url <- paste0("https://trackmania.exchange/mapgbx/", id)
-  name <- get_map_name(id)
-  output_path <- paste0(name, ".Map.Gbx")
+download_map <- function(ids) {
+  for (id in ids) {
+    # Define url and output path
+    url <- paste0("https://trackmania.exchange/mapgbx/", id)
+    name <- get_map_name(id)
+    output_path <- paste0(name, ".Map.Gbx")
 
-  # Send GET request
-  response <- httr::GET(url, httr::add_headers(`User-Agent` = "trackmaniar/1.0"),
-                       httr::write_disk(output_path, overwrite = TRUE))
+    # Send GET request
+    response <- httr::GET(url, httr::add_headers(`User-Agent` = "trackmaniar/1.0"),
+                         httr::write_disk(output_path, overwrite = TRUE))
 
-  # Check the result
-  if (httr::status_code(response) == 200) {
-    message("Map downloaded successfully to: ", output_path)
-  } else {
-    warning("Failed to download map. Status code: ", httr::status_code(response))
+    # Check the result
+    if (httr::status_code(response) == 200) {
+      message("Map downloaded successfully to: ", output_path)
+    } else {
+      warning("Failed to download map. Status code: ", httr::status_code(response))
+    }
   }
 }
--- R/search_maps.R
+++ R/search_maps.R
@@ -1,5 +1,14 @@
 #' @export
-search_maps <- function(params) {
+search_maps <- function(params, fields) {
+  # Define fields if not provided
+  if (missing(fields)) {
+    fields <- "Name,MapId,Authors[],MapType,Environment,Vehicle,Difficulty"
+    fields <- paste0(fields, ",AwardCount,UploadedAt,OnlineWR[]")
+    fields <- paste0(fields, ",DownloadCount,Tags[]")
+  }
+  params <- as.list(params)
+  params$fields <- fields
+
   # Define url
   base_url <- "https://trackmania.exchange/api/maps?"
   full_url <- httr::modify_url(base_url, query = params)
@@ -11,8 +20,26 @@ search_maps <- function(params) {
   if (httr::status_code(response) == 200) {
     # Parse the JSON content
     data <- jsonlite::fromJSON(httr::content(response, as = "text", encoding = "UTF-8"))
-    return(data)
+    df <- as.data.frame(data$Results)
   } else {
     warning("Request failed with status code: ", httr::status_code(response))
   }
+
+  # Split author column up if it exists
+  if (stringr::str_detect(fields, "Authors")) {
+    df <- df |>
+      tibble::add_column(AuthorId = paste(df$Authors[[1]]$User$UserId, collapse = " "), .after = 1) |>
+      tibble::add_column(AuthorName = paste(df$Authors[[1]]$User$Name, collapse = " "), .after = 2) |>
+      tibble::add_column(AuthorRole = paste(df$Authors[[1]]$Role, collapse = " "), .after = 3) |>
+      dplyr::select(-Authors)
+  }
+
+  print(colnames(df))
+  # Edit WR columns
+  if (stringr::str_detect(fields, "OnlineWR")) {
+    df <- df |>
+      dplyr::select(-c("OnlineWR$AccountId", "OnlineWR$User$Name"))
+  }
+
+  return(df)
 }
